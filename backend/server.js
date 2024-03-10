a01fa1a412765ba556d8c1922d545ece02eb9036
saeed <me.coderr@gmail.com>
2024-03-10 18:57:58 +0530

    about to deploy

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,14 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
-const { chats } = require("./data");
 const { connectDB } = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
-const User = require("./models/userModel");
+const path = require("path");
 
 const app = express();
-
-const popul = (chat) => {};
 
 connectDB();
 app.use(express.json());
@@ -22,13 +19,29 @@ app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
-
 app.get("/api/home", (req, res) => {
   res.send("API is running");
 });
+
+//  ---------------- deploymeny --------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname1, "..", "frontend", "dist", "index.html")
+    );
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running");
+  });
+}
+
+// ------------------------------------------------
 
 app.use(notFound);
 app.use(errorHandler);

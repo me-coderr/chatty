adb00e7cdc8ed45f4ec63001bfe34ac7c990ad6a
saeed <me.coderr@gmail.com>
2024-03-14 01:23:05 +0530

    will now work

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:5183/api",
+        // target: "http://127.0.0.1:5183/api",
+        target: "https://chatty-2ikm.onrender.com/api",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
